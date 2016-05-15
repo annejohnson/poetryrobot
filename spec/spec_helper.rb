932c171c118a77f79343a71d5afddf18bf7a2581
Anne <annekjohnson23@yahoo.com>
2016-05-14 21:37:56 -0400

    Create TwitterWrapper class & add specs with VCR-mocked requests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require './lib/hash_cleaner.rb'
 require './lib/string_cleaner.rb'
 require './lib/tweet_filterer.rb'
+require './lib/twitter_wrapper.rb'
 require './lib/poetry_foundation_random_poem_scraper.rb'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :faraday
+end
