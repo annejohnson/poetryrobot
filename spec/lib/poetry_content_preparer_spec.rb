496fe6f2f3f9ae3ba462e24360cea8bb2a2b041a
Anne <annekjohnson23@yahoo.com>
2016-05-15 19:59:44 -0400

    Add TwitterContentPreparer base class & PoetryContentPreparer child class
    - Add associated spec file

---
+++ spec/lib/poetry_content_preparer_spec.rb
@@ -0,0 +1,44 @@
+require './spec/spec_helper.rb'
+
+shared_examples 'a tweet' do
+  let(:max_num_characters) { 140 }
+
+  it 'contains the correct number of characters' do
+    expect(tweet.length <= max_num_characters).to eq(true)
+  end
+end
+
+describe PoetryContentPreparer do
+  describe '#prepare_tweet' do
+    let(:tweet) { subject.prepare_tweet }
+
+    it_behaves_like 'a tweet'
+
+    it 'contains a url' do
+      expect(tweet).to match(/https?:\/\//i)
+    end
+
+    it 'contains an author attribution' do
+      expect(tweet).to match(/\S by \S/i)
+    end
+  end
+
+  describe '#prepare_reply' do
+    let(:tweet_to_reply_to) { 'Roses are red, violets are blue #poetry' }
+    let(:user_to_reply_to) { 'poetryrobot' }
+
+    let(:tweet) { subject.prepare_reply(tweet_to_reply_to, user_to_reply_to) }
+
+    it_behaves_like 'a tweet'
+
+    it 'contains the user\'s twitter handle' do
+      expect(tweet).to match("@#{user_to_reply_to}")
+    end
+  end
+
+  describe '#topic_search_string' do
+    it 'is a poetry-related hashtag' do
+      expect(subject.topic_search_string).to match(/\A#poe.*/)
+    end
+  end
+end
