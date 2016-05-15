e6366eb8c01d3cd09a924409aed5d723a6a62711
Anne <annekjohnson23@yahoo.com>
2016-05-14 23:45:40 -0400

    Create not-yet-implemented specs

--- spec/lib/poetry_robot_spec.rb
+++ spec/lib/poetry_robot_spec.rb
@@ -2,21 +2,27 @@ require './spec/spec_helper.rb'
 
 describe PoetryRobot do
   describe '#tweet_poem' do
+    it 'tweets a poem'
   end
 
   describe '#retweet_a_poet' do
+    it 'retweets a poet'
   end
 
   describe '#retweet_mentions' do
+    it 'retweets mentions'
   end
 
   describe '#follow_a_poet' do
+    it 'follows a poet'
   end
 
   describe '#reply_to_a_poet' do
+    it 'replies to a poet'
   end
 
   describe '#follow_followers' do
+    it 'follows its followers'
   end
 
   describe '#create_poem_tweet' do
