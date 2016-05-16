2783ee2574a7ec9c88c312902ba3b0e0bfd255e1
Anne <annekjohnson23@yahoo.com>
2016-05-16 19:45:23 -0400

    Extract out PoetryFoundationScraperSpec tests into a poem_scraper shared example

--- spec/lib/poetry/scraper/poetry_foundation_scraper_spec.rb
+++ spec/lib/poetry/scraper/poetry_foundation_scraper_spec.rb
@@ -1,17 +1,5 @@
 require './spec/spec_helper.rb'
 
 describe Poetry::Scraper::PoetryFoundationScraper do
-  describe '#get_poem_hash' do
-    let(:result) { subject.get_poem_hash }
-
-    it 'contains title, author, and URL strings' do
-      [:title, :author, :url].each do |k|
-        expect(result[k]).to be_a String
-      end
-    end
-
-    it 'contains an array of lines' do
-      expect(result[:lines]).to be_a Array
-    end
-  end
+  it_behaves_like 'a poem scraper'
 end
--- spec/shared_examples.rb
+++ spec/shared_examples.rb
@@ -5,3 +5,19 @@ shared_examples 'a tweet' do
     expect(tweet.length <= max_num_characters).to eq(true)
   end
 end
+
+shared_examples 'a poem scraper' do
+  describe '#get_poem_hash' do
+    let(:result) { subject.get_poem_hash }
+
+    it 'contains title, author, and URL strings' do
+      [:title, :author, :url].each do |k|
+        expect(result[k]).to be_a String
+      end
+    end
+
+    it 'contains an array of lines' do
+      expect(result[:lines]).to be_a Array
+    end
+  end
+end
